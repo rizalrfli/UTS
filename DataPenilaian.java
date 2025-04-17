c7afa347a548207166e3d4c852120e53649cc560
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2025-04-17 21:24:03 +0700

    input array

--- DataPenilaian.java
+++ DataPenilaian.java
@@ -1,7 +1,8 @@
 public class DataPenilaian {
     Mahasiswa [] datamhs = new Mahasiswa [3];
-    int idx;
-    void tampil() {
+    MataKuliah [] datamk = new MataKuliah [3];
+    Penilaian [] datapen = new Penilaian [3];
+    void tampilmhs() {
         for (Mahasiswa m : datamhs) {
             m.tampilMahasiswa();
         }
