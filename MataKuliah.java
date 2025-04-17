fe3841613363ffc293b6d8fcd7bb53fd6276e0a1
Rizkihadiono <rizkihadiono95@gmail.com>
2025-04-17 21:12:52 +0700

    buat class matakuliah

--- MataKuliah.java
+++ MataKuliah.java
@@ -1,4 +1,27 @@
-public class MataKuliah {
+class MataKuliah {
+    private String kodeMK;
+    private String namaMK;
+    private int sks;
 
-    
+    public MataKuliah(String kodeMK, String namaMK, int sks) {
+        this.kodeMK = kodeMK;
+        this.namaMK = namaMK;
+        this.sks = sks;
+    }
+
+    public String getKodeMK() {
+        return kodeMK;
+    }
+
+    public String getNamaMK() {
+        return namaMK;
+    }
+
+    public int getSks() {
+        return sks;
+    }
+
+    public void tampilMataKuliah() {
+        System.out.println("Kode MK: " + kodeMK + " | Nama: " + namaMK + " | SKS: " + sks);
+    }
 }
